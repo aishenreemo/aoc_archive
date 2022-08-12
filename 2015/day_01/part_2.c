39565267687c50b3670a284e1d20ad7f8ceb54b1
Aivan <aish3n@pm.me>
2022-08-12 15:21:37 +0800

    feat: solve part 2 of day 1, 2015

---
+++ 2015/day_01/part_2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../../include/load_file.h"
+
+int main() {
+	char *input = load_file("2015/day_01/input.txt");
+
+	int length = strlen(input);
+	int floor_index = 0;
+
+	for (int i = 0; i < length; i += 1) {
+		char *c = input + i;
+
+		switch (*c) {
+
+		case '(':
+			floor_index += 1;
+			break;
+		case ')':
+			floor_index -= 1;
+			break;
+		}
+
+		if (floor_index < 0) {
+			printf("Santa got on the basement for %d turns\n", i + 1);
+			break;
+		}
+	}
+
+	free(input);
+	return 0;
+}
